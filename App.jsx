b30dc0e66c86466b11dee73782aa53e5c1a81945
RakhiRajesh <86522793+RakhiRajesh@users.noreply.github.com>
2021-06-26 19:05:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+import Cyber from "./Cyber";
+import Full from "./Full";
+import Data from "./Data";
+import Python from "./Python";
+function App(){
+  return (
+    <div>
+    <Header />
+    <Footer />
+    <Info />
+    <Cyber />
+    <Full />
+    <Data />
+    <Python />
+    </div>
+    );
+}
+
+export default App;
+
